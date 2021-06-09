107299b1b99d1e58d6f2105269452c0f9fa80576
tanselsoner <82914615+tanselsoner@users.noreply.github.com>
2021-06-09 08:15:35 +0600

    lyft

--- src/day_51_inheritance/super_keyword/LyftRides.java
+++ src/day_51_inheritance/super_keyword/LyftRides.java
@@ -4,9 +4,12 @@ public class LyftRides {
 
     public static void main(String[] args) {
         Lyft lyftRide = new Lyft();
-        LyftXL LyftXlRide = new LyftXL();
-        Lux LuxRide = new Lux();
-        System.out.println();
+        LyftXL lyftXlRide = new LyftXL();
+        Lux luxRide = new Lux();
+        System.out.println("lyftRide for 5 miles = $" + lyftRide.calculateRate(5));
+        System.out.println("lyftXLRide for 5 miles = $" +  lyftXlRide.calculateRate(5));
+        System.out.println("luxRide for 5 miles = $" + luxRide.calculateRate(5));
+
 
 
     }
