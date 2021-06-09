2f0397b754c025eebf8663d6a10a8fc911316ae7
tanselsoner <82914615+tanselsoner@users.noreply.github.com>
2021-06-09 08:08:35 +0600

    lyft

---
+++ src/day_51_inheritance/super_keyword/LyftRides.java
@@ -0,0 +1,13 @@
+package day_51_inheritance.super_keyword;
+
+public class LyftRides {
+
+    public static void main(String[] args) {
+        Lyft lyftRide = new Lyft();
+        LyftXL LyftXlRide = new LyftXL();
+        Lux LuxRide = new Lux();
+        System.out.println();
+
+
+    }
+}
